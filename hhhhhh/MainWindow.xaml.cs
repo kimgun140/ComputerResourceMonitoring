8d181f0d2b3a96e97e0d7ce6666376b2c7c3bd65
건 김 <kimgun140@gmail.com>
2024-06-05 21:20:52 +0900

    stag

--- hhhhhh/MainWindow.xaml.cs
+++ hhhhhh/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static hhhhhh.NewFolder1.Computerdata;
 using MySql.Data.MySqlClient;
+using System.Windows.Threading;
 
 namespace hhhhhh
 {
@@ -43,40 +44,134 @@ namespace hhhhhh
 
         public class db
         {
-            public void compu()
+            private string ip;
+            private int port;
+            private string pwd;
+            private string dbname;
+            private string uid;
+            private string connectString;
+            MySqlConnection conn = null;
+
+            public db()
             {
-               MySqlConnection conn = null;
                 string ip = "127.0.0.1";
                 int port = 3306;
                 string uid = "Student";
                 string pwd = "1234";
-                string dbname = "cpu_data";
+                string dbname = "com";
                 string connectString = $"Server={ip};Port={port};Database={dbname};uid={uid};pwd={pwd};CharSet=utf8;";
-                //연결 확인 
                 conn = new MySqlConnection(connectString);
-                conn.Open();
-                conn.Ping();
-                //조회할 쿼리문 
-                string query = "SELECT * FROM cpu_data";
-                MySqlDataReader dr = null;
-                //쿼리 담을 LIst
-                List<string> result = new List<string>();
-                //쿼리 명령 실행
-            MySqlCommand cmd = new MySqlCommand(query, conn);
 
-                //조회 결과
-                dr  = cmd.ExecuteReader();
-                while (dr.Read())
-                {
-                    //데이터 조회시 null 값이 있을 경우에는 예외처리 필요.
-                    result.Add($"{(dr[0].ToString())},{dr[1].ToString()}");
-                }
-                foreach (string item in result)
-                {
-                    Console.WriteLine(item);
-                }
-            
             }
+            public string cpudata(int cpuvalue)
+            {
+                try
+                {
+                    //연결 확인 
+                    conn.Open();
+                    conn.Ping();
+                    //실행할 쿼리문 
+                    string query = "INSERT INTO cpu_data VALUES (DEFAULT," + cpuvalue + ");";
+                    //쿼리 명령 실행
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();// 결과 집합이 없는  sql 전송 (적용된 행의 수 리턴)
+                    conn.Close();
+                    return "ok";
+
+                }
+                catch (MySqlException e)
+                {
+                    return e.Message; ;
+
+                }
+
+            }
+            public string memdata(int memvalue)
+            {
+                try
+                {
+                    //연결확인
+                    conn.Open();
+                    conn.Ping();
+                    // 실행할 쿼리문 
+                    string query = "INSERT INTO mem_data VALUES (DEFAULT, " + memvalue + ");";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return "memok";
+
+                }
+                catch (MySqlException e)
+                {
+                    return e.Message;
+
+                }
+            }
+            public string procdata(double procvalue)
+            {
+                try
+                {
+                    //연결확인
+                    conn.Open();
+                    conn.Ping();
+                    // 실행할 쿼리문 
+                    string query = "INSERT INTO proc_data VALUES (DEFAULT, " + procvalue + ");";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return "procok";
+
+                }
+                catch (MySqlException e)
+                {
+                    return e.Message;
+
+                }
+            }
+            //public string selectdata(int cpuvalue)
+            //{
+            //    try
+            //    {
+            //        //연결 확인 
+            //        conn.Open();
+            //        conn.Ping();
+            //        //실행할 쿼리문 
+            //        //string query = "INSERT INTO cpu_data VALUES (DEFAULT," + cpuvalue + ");";
+            //        string query = "SELECT * FROM cpu_data;";
+
+            //        //string query = "INSERT INTO cpu_data VALUES (date_format(NOW(), '%Y-%m-%d-%H-%i-%s')," + cpuvalue + ");";
+
+            //        MySqlDataReader dr = null;
+            //        //쿼리 담을 LIst
+            //        List<string> result = new List<string>();
+            //        //dataset를 사용하면 데이터 베이스를 통째로 가져올 수 있겠네 dataset안에 datatable의 이름으로 검색해서 datarow에 접근 할 수 있겠네 ?
+            //        //쿼리 명령 실행
+            //        MySqlCommand cmd = new MySqlCommand(query, conn);
+            //        //cmd.ExecuteNonQuery();// 결과 집합이 없는  sql 전송 (적용된 행의 수 리턴)
+            //        dr = cmd.ExecuteReader(); //결과 집합이 있는 sql 전송(결과 집합을 담은 reader 객체 리턴 (select)
+
+            //        //조회 결과 select 문 쓸때 
+            //        //while (dr.Read())
+            //        //{
+            //        //    //데이터 조회시 null 값이 있을 경우에는 예외처리 필요.
+            //        //    result.Add($"{(dr[0].ToString())},{dr[1].ToString()}");
+            //        //}
+            //        //foreach (string item in result)
+            //        //{
+            //        //    Console.WriteLine(item);
+            //        //}
+            //        conn.Close();
+            //        return "ok";
+
+            //        //return 0;
+            //    }
+            //    catch (MySqlException e)
+            //    {
+            //        return e.Message; ;
+
+            //    }
+
+            //}
         }
 
         public MainWindow()
@@ -84,6 +179,7 @@ namespace hhhhhh
             InitializeComponent();
             _timer = new System.Timers.Timer();
             _timer.Interval = 3000;
+            background();
             _timer.Elapsed += OntimedEvent;
             _timer.AutoReset = true;
             _timer.Enabled = true;
@@ -96,9 +192,9 @@ namespace hhhhhh
             Chart1.DataContext = this;
             Chart2.DataContext = this;
 
-
-
         }
+        db cpudb = new db();//  디비 객체를 여기 만들어야하네
+
         public void OntimedEvent(object source, System.Timers.ElapsedEventArgs e)
         {
             int cpuvalue = GetCpuValue();
@@ -124,7 +220,7 @@ namespace hhhhhh
                 ProgressBarCpu.Dispatcher.BeginInvoke((System.Action)(() =>
                 {
                     ProgressBarCpu.Value = cpuvalue;
-                    cpuuu.Content = cpuvalue ;
+                    cpuuu.Content = cpuvalue;
                     C_Chart.Add((int)cpuvalue);
                 }));
             }
@@ -144,7 +240,7 @@ namespace hhhhhh
 
                 }));
             }
-            if(ProgressProcess.Dispatcher.CheckAccess())
+            if (ProgressProcess.Dispatcher.CheckAccess())
             {
                 ProgressProcess.Value = processvalue;
                 processsss.Content = processvalue.ToString();
@@ -159,6 +255,8 @@ namespace hhhhhh
                     P_Chart.Add((int)processvalue);
                 }));
             }
+
+
 
         }
 
@@ -181,7 +279,7 @@ namespace hhhhhh
 
 
         public double GetProcessCpuUsage()
-            //
+        //
         {
             try
             {
@@ -197,6 +295,45 @@ namespace hhhhhh
             }
         }
 
+        private async void background()
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    int cpuvalue = GetCpuValue();
+                    string a = cpudb.cpudata(cpuvalue);
+                    //cpudatatext.Text = a;
+                    //Dispatcher.BeginInvoke((Action)(async () =>
+                    //    {
+                    //        await Task.Run(async () =>
+                    //        {
+                    //            cpudatatext.Text = a;
+                                
+                    //        });
+                    //    }));
+                    int memvalue = GetMemValue();
+                    string b = cpudb.memdata(memvalue);
+                    double procvalue = GetProcessCpuUsage();
+                    string c = cpudb.procdata(procvalue);
 
+                    await Task.Delay(3000);
+                }
+            });
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int cpuvalue = GetCpuValue();
+            string a = cpudb.cpudata(cpuvalue);
+            cpudatatext.Text = a;
+            int memvalue = GetMemValue();
+            string b = cpudb.memdata(memvalue);
+            memdatatext.Text = b;
+            double procvalue = GetProcessCpuUsage();
+            string c = cpudb.procdata(procvalue);
+            procdatatext.Text = c;
+        }
     }
 }
